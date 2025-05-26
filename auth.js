22d5f7f9c9749baac09203c416306da71f13f41e
محمود فوزى احمد عجيز <UG_31117165@pharm.tanta.edu.eg>
2025-05-26 18:27:46 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const { TelegramApi } = require('telegram');
 const { StringSession } = require('telegram/sessions');
 const readline = require('readline');
 
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
